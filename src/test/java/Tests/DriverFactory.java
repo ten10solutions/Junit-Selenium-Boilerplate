b501c33883ff93898c7349986d918e642271a304
Peter Russell <qwertyface@gmail.com>
2018-04-06 09:10:30 +0100

    Reinstate Webdriver manager. Bump Selenium Version.

--- src/test/java/Tests/DriverFactory.java
+++ src/test/java/Tests/DriverFactory.java
@@ -1,23 +1,25 @@
 package Tests;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Created by jack.forman on 22/10/2016.
- */
 public class DriverFactory {
+
+    static {
+        ChromeDriverManager.getInstance().setup();
+    }
 
     private WebDriver driver;
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         if (driver == null) {
             setDriver();
         }
         return driver;
     }
 
-    private void setDriver(){
+    private void setDriver() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
