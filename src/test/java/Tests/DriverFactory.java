96711cfbcd8af761b2dfa3e2ddcb776309848884
Peter Russell <qwertyface@gmail.com>
2019-07-11 09:22:48 +0100

    (and fix the import)

--- src/test/java/Tests/DriverFactory.java
+++ src/test/java/Tests/DriverFactory.java
@@ -1,6 +1,6 @@
 package Tests;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
