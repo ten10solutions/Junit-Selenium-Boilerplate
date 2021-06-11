b708572d0bb9c0ec684327320228d9a4e5db6807
Peter Russell <qwertyface@gmail.com>
2021-06-11 09:34:54 +0100

    Update HomePage.java

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
-    private static String URL = "http://35.177.253.126/index.php";
+    private static String URL = "http://3.14.228.156/";
     private static final By SIGN_IN_BUTTON = By.cssSelector("a.login");
 
    public void goTo(){
