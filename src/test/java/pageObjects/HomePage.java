07fdcbb6b125f43dfb94a5b08701e4304ec85df3
Peter Russell <qwertyface@gmail.com>
2021-10-13 09:34:55 +0100

    Update HomePage.java
    Change URL

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
-    private static String URL = "http://3.14.228.156/";
+    private static String URL = "http://35.179.99.144/";
     private static final By SIGN_IN_BUTTON = By.cssSelector("a.login");
 
    public void goTo(){
